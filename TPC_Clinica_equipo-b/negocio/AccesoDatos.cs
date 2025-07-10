852081c2b1de4730018669c0b9be7cc2fde54c65
FerG0373 <fer.dg.sc@gmail.com>
2025-07-10 08:31:40 -0300

    Se agregaró SqlTransaction como propiedad en Acceso a datos y métodos; se agregó label de error en FormularioMedico.aspx; se modificó SP

--- TPC_Clinica_equipo-b/negocio/AccesoDatos.cs
+++ TPC_Clinica_equipo-b/negocio/AccesoDatos.cs
@@ -12,9 +12,15 @@ namespace negocio
         private SqlConnection conexion;
         private SqlCommand comando;
         private SqlDataReader lector;
+        private SqlTransaction transaccion;
+
         public SqlDataReader Lector
         {
             get { return lector; }
+        }
+        public SqlTransaction Transaccion
+        {
+            get { return transaccion; }
         }
 
         public AccesoDatos()
@@ -65,7 +71,15 @@ namespace negocio
 
             try
             {
-                conexion.Open();
+                if(transaccion != null)
+                {
+                    // Ya fue abierta por iniciarTransaccion()
+                    comando.Transaction = transaccion;
+                }
+                else if(conexion.State != System.Data.ConnectionState.Open)
+                {
+                    conexion.Open(); // Solo abrí si no está abierta
+                }
                 comando.ExecuteNonQuery();
             }
             catch (Exception ex)
@@ -80,13 +94,36 @@ namespace negocio
             comando.Parameters.Clear();  // Para limpiar el SqlCommand.
         }
 
+        public void iniciarTransaccion()
+        {
+            if(conexion.State != System.Data.ConnectionState.Open)
+                conexion.Open();
+
+            transaccion = conexion.BeginTransaction();
+            comando.Transaction = transaccion;
+        }
+
+        public void confirmarTransaccion()
+        {
+            if(transaccion != null)
+                transaccion.Commit();
+        }
+
+        public void cancelarTransaccion()
+        {
+            if(transaccion != null)
+                transaccion.Rollback();
+        }
+
 
         public void cerrarConexion()
         {
-            if (lector != null)
-            {
+            if(lector != null)
                 lector.Close();
-            }
+
+            if(transaccion != null)
+                transaccion.Dispose();
+
             conexion.Close();
         }
 
